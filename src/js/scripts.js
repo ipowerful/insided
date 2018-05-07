f07fa5c3aa59b5d525362d75188e91cfa3b59c44
Ilya <eley@mail.ru>
2018-05-07 07:29:30 +0300

    Add dropdown

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,32 @@
 // Sidebar Navigation
 //
 
+$('.dropdown__toggle').click(function(e) {
+  e.preventDefault();
+
+  $(this).toggleClass('is-active');
+  $(this).parent().find('.dropdown-menu').toggle();
+});
+
+$('.dropdown-menu > li > a').click(function(e) {
+  var name = $(this).html();
+  var $dropdown = $(this).parents('.dropdown');
+  var $dropdown__toggle = $dropdown.find('.dropdown__toggle');
+  var $dropdown__menu = $dropdown.find('.dropdown-menu');
+
+  $dropdown__toggle.html(name);
+  $dropdown__toggle.addClass('is-selected');
+  $dropdown__menu.hide();
+});
+
+
+//
+// Sidebar Navigation
+//
+
 $('.sidebar__nav .js-has-subnav').click(function(e) {
   e.preventDefault();
 
-  $(this).toggleClass('active');
+  $(this).toggleClass('is-active');
   $(this).parent().find('.sidebar__subnav').toggle();
 });
