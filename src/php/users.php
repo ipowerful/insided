6fff01a930c6b84966de1bc527d3d08cbf13fed6
Ilya <eley@mail.ru>
2018-05-10 08:29:33 +0300

    Add table sorting

--- src/php/users.php
+++ src/php/users.php
@@ -1,33 +1,48 @@
 <?php
 
+// In actuall project data should sorted by DB.
+// I imitated DB by creating array_orderby() function.
+// Basing on this sorting by date fields in current implementation is incorrect.
+
 ini_set('display_errors', 0);
 
 function array_orderby()
 {
-    $args = func_get_args();
-    $data = array_shift($args);
-    foreach ($args as $n => $field) {
-        if (is_string($field)) {
-            $tmp = array();
-            foreach ($data as $key => $row)
-                $tmp[$key] = $row[$field];
-            $args[$n] = $tmp;
-            }
-    }
-    $args[] = &$data;
-    call_user_func_array('array_multisort', $args);
-    return array_pop($args);
+   $args = func_get_args();
+   $data = array_shift($args);
+   foreach ($args as $n => $field) {
+      if (is_string($field)) {
+         $tmp = array();
+         foreach ($data as $key => $row)
+            $tmp[$key] = $row[$field];
+         $args[$n] = $tmp;
+         }
+   }
+   $args[] = &$data;
+   call_user_func_array('array_multisort', $args);
+   return array_pop($args);
 }
 
 $json = file_get_contents('../data/users.json');
 $data = json_decode($json, true);
 
 
-$sorted = array_orderby($data, 'solved', SORT_ASC);
-//print_r($sorted);
+$column = filter_var($_GET['column'], FILTER_SANITIZE_STRING);
+$dir = filter_var($_GET['dir'], FILTER_SANITIZE_STRING);
+
+if( ($column != '') && ($dir != '') ) {
+
+   // here is column var checks ...
+
+   $sortDir = ($dir == 'asc') ? SORT_ASC : SORT_DESC;
+
+   $sortedData = array_orderby($data, $column, $sortDir);
+} else {
+   $sortedData = $data;
+}
 
 
-echo json_encode($sorted);
+echo json_encode($sortedData);
 
 
 ?>
